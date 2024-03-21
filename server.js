c383426914d77fcdbed5ebda857f8a0ed1b36c0c
CesarRyudi <cesarryudi@gmail.com>
2024-03-21 20:05:58 -0300

    CORS

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 require("dotenv").config();
+const cors = require("cors");
+
 const clientesRoutes = require('./src/Clientes/routes')
 const produtosRoutes = require("./src/Produtos/routes");
 const vendasRoutes = require("./src/Vendas/routes");
@@ -8,6 +10,18 @@ const app = express();
 const port = process.env.PORT;
 
 app.use(express.json());
+
+const allowedOrigins = [
+  "https://haru-cookies.vercel.app/",
+  "http://localhost:5173/",
+];
+
+const corsOptions = {
+  origin: allowedOrigins,
+};
+app.use(cors(corsOptions));
+
+
 
 app.get("/", (req, res) => {
     res.status(200).send("Hello world!");
